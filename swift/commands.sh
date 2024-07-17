0bd6fa889f9c218ac54227c1bde9437b83bd47fa
AshrafMah <ashraf030780@gmail.com>
2024-07-17 04:13:03 +0300

    Change Port

--- swift/commands.sh
+++ swift/commands.sh
@@ -9,3 +9,6 @@ python WeaviateIngestion/create-suggestion-schema.py
 
 # Load and chunk data to import into Weaviate (Using Haystrack processing)
 python WeaviateIngestion/import_weaviate.py
+
+# Start API
+uvicorn api:app --reload --host 0.0.0.0 --port 8001
